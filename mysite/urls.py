38528d72a6e163ab2a5507a6ed7e468b9958a26e
Syed <syedbdfoods@gmail.com>
2020-06-14 14:51:37 +0800

    Show all blog posts on the home page.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+     # 127.0.0.1:8000
+    path('', include('blog.urls'))
 ]
