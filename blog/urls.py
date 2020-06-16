219abd8c6d88da4b0b5cdefa30d16ec9de23e94e
Syed <syedbdfoods@gmail.com>
2020-06-16 10:55:36 +0800

    Style the app with bootstrap and css. Also, apply DRY principle by us…

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
 
     # 127.0.0.1:8000/post/2 --> local
     # mydjangosite.com/post/2 --> online
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
