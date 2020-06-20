c8f4930df8bb785853684e068509484abd7585b8
Syed <syedbdfoods@gmail.com>
2020-06-20 12:15:28 +0800

    Add markdown and syntax highlighting to blog posts and comments.

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     # mydjangosite.com/post/2 --> online
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 
-     # 127.0.0.1:8000/post/new --> local
+    # 127.0.0.1:8000/post/new --> local
     # mydjangosite.com/post/new --> online
     path('post/new/', views.post_new, name='post_new'),
 
@@ -33,12 +33,12 @@ urlpatterns = [
     # mydjangosite.com/post/2/comment --> online
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 
-     # 127.0.0.1:8000/comment/2/remove --> local
+    # 127.0.0.1:8000/comment/2/remove --> local
     # mydjangosite.com/comment/2/remove --> online
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),  
 
 
-     # 127.0.0.1:8000/comment/2/approve --> local
+    # 127.0.0.1:8000/comment/2/approve --> local
     # mydjangosite.com/comment/2/approve --> online
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
 
